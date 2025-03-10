b86aa9b2329e76efe0eb876c1dfb562b5a6c8019
aboadiag <136507019+aboadiag@users.noreply.github.com>
2025-03-10 12:35:57 -0400

    base

--- netlify/functions/ea_backend.py
+++ netlify/functions/ea_backend.py
@@ -40,4 +40,4 @@ def log_Slider_data():
     # Run the Flask app
 if __name__ == "__main__":
     # start_interaction() # intiialize the misty
-    app.run(debug=True, port=80) #flask should listen here
+    app.run(host="0.0.0.0", port=80) #flask should listen here
